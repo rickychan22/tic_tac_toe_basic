94bfe0cf3449b8f0e831ed78642b697d2a7d04a5
Ricky Chan <chan.ricky.22@hotmail.com>
2020-12-21 19:25:27 -0500

    implemented player input and display on board

--- game_func.py
+++ game_func.py
@@ -19,12 +19,12 @@ dic_board_val = {7: ' ', 8:' ', 9: ' ',
 
 #promt the user if they want to be X or O
 def player_input():
-	player_choice = ''
+	player_marker = ''
 
-	while not (player_choice == 'X' or player_choice =='O'):
-		player_choice  = input('Player1: Would you like to be X or O?').upper()
+	while not (player_marker == 'X' or player_marker == 'O'):
+		player_marker = input('Player 1: Do you want to be X or O? ').upper()
 
-	if player_choice == 'X':
+	if player_marker == 'X':
 		return ('X','O')
 	else:
 		return ('O','X')
@@ -37,6 +37,21 @@ def who_goes_first():
 		return 'Player 1'
 	else:
 		return 'Player 2'
+
+#check if location on board is empty
+def check_space(board, position):
+	if board[position] == ' ':
+		return True
+	else:
+		return False
+
+#promt the player to pick a location and validate if this location is available
+def player_choice(board):
+	position = 0
+
+	while position not in [1,2,3,4,5,6,7,8,9] or check_space(board, position) == False:
+		position = int(input('Where would you like to place you marker 1-9?'))
+	return position
 
 #function to check if there is a winner
 def win_checker():
--- game_main.py
+++ game_main.py
@@ -1,12 +1,47 @@
 from board_generator import board
 import game_func
 
-print('WELCOME TO THE GAME')
 
+def yes_no_validation():
+	ready = input('Are you ready to play? Please enter Y or N').upper()
+
+	while not (ready == 'Y' or ready == 'N'):
+		ready = input('Please enter Y or N').upper()
+
+	return ready
+
+print('WELCOME TO THE GAME')
 
 while True:
 	#set the board to empty
 	board_new = [' '] * 10
-	player1_marker = game_func.player_input()
+
+	#who goes first and set X or O
+	player1_marker, player2_marker = game_func.player_input()
 	turn = game_func.who_goes_first()
 	print(turn + ' will go first.')
+
+	game_on = yes_no_validation()
+
+	if game_on == 'Y':
+		play = True
+	else:
+		play = False
+
+	while play:
+
+		#player 1 turn
+		if turn == 'Player 1':
+			board(board_new)
+			position = game_func.player_choice(board_new)
+			game_func.place_marker(board_new,player1_marker,position)
+			print(turn)
+			turn = 'Player 2'
+
+		
+		#player 2 turn
+		else:
+			board(board_new)
+			position = game_func.player_choice(board_new)
+			game_func.place_marker(board_new,player2_marker,position)
+			turn = 'Player 1'
